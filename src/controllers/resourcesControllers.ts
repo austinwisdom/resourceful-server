e9d4d8ee61b24adc2f40c5fac717d549f132de33
JeffOrdze <jeff.ordze@live.ca>
2023-07-16 14:43:24 -0600

    refactor resources query to only accept category and test data with db entries that were just populated

--- src/controllers/resourcesControllers.ts
+++ src/controllers/resourcesControllers.ts
@@ -1,6 +1,6 @@
 import { ObjectId } from "mongodb";
 import { collections } from "../db/conn";
-import express, { Request, Response, NextFunction} from "express";
+import express, { Request, Response} from "express";
 import Resources from "../models/resources";
 const mongo = require("mongodb");
 
@@ -8,8 +8,8 @@ const getResources = async (req:Request, res:Response) => {
     try {
         const resources = (
             await collections.resources!
-            .find({category: req.params.category.toLowerCase(), subcategory: req.params.subcategory.toLowerCase()}).toArray()) as Resources[]
-         res.status(200).send(resources);
+            .find({category: req.params.category}).toArray()) as Resources[]
+         res.status(200).json(resources);
      } catch (error) {
          res.status(500).send((error as Error).message);
      }
