12a832e8c650e6d5f6ec17c8a7e39ee67e17e368
JeffOrdze <jeff.ordze@live.ca>
2023-07-27 16:45:45 -0600

    add authentication for resources routes to protect them

--- src/routes/resourcesRoutes.ts
+++ src/routes/resourcesRoutes.ts
@@ -1,6 +1,7 @@
 const resourcesRouter = require('express').Router()
 const resourcesControllers = require('../controllers/resourcesControllers')
+const usersMiddleware = require('../controllers/usersControllers')
 
-resourcesRouter.route("/:category").get(resourcesControllers.getResources)
+resourcesRouter.route("/:category").get(usersMiddleware.authUser, resourcesControllers.getResources)
 
 module.exports = resourcesRouter
